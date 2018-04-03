eeebaa937bb151a0ac4325cebd296b96f37135ae
xp-vit <xp.vit.blr@gmail.com>
2018-04-03 14:25:53 +0200

    print coverage calculated

--- vars/postJacocoCoverage.groovy
+++ vars/postJacocoCoverage.groovy
@@ -1,15 +1,16 @@
 #!/usr/bin/env groovy
 
 def call(Map args) {
-  final Double coverageThreshold = args.threshold ?: 80.0
-  final Double coverageDeltaThreshold = args.deltaThreshold ?: null
-  final String xmlPath = args.xmlPath ?: "target/site/jacoco/jacoco.xml"
+    final Double coverageThreshold = args.threshold ?: 80.0
+    final Double coverageDeltaThreshold = args.deltaThreshold ?: null
+    final String xmlPath = args.xmlPath ?: "target/site/jacoco/jacoco.xml"
 
-  final lib = new com.spotify.jenkinsfile.Coverage()
+    final lib = new com.spotify.jenkinsfile.Coverage()
 
-  final Double coverage = lib.getCoverageFromJacoco(xmlPath)
-  lib.postCoverage(coverage, coverageThreshold)
+    final Double coverage = lib.getCoverageFromJacoco(xmlPath)
+    System.out.println("Current coverage:" + coverage)
+    lib.postCoverage(coverage, coverageThreshold)
 
-  final Double coverageDelta = lib.getCoverageDelta()
-  lib.postCoverageDelta(coverageDelta, coverageDeltaThreshold)
+    final Double coverageDelta = lib.getCoverageDelta()
+    lib.postCoverageDelta(coverageDelta, coverageDeltaThreshold)
 }
